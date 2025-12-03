ac63be1cdb365c58a65a9095eeca8bbbd495ec11
chuchoeientech <carlos@eientech.io>
2025-12-03 14:36:57 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { client, urlFor } from './sanityClient';
 import Navbar from './components/global/Navbar';
 import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
@@ -35,6 +37,29 @@ function AnimatedRoutes() {
 
 function App() {
   const { scrollYProgress } = useScroll();
+
+  useEffect(() => {
+    const updateFavicon = async () => {
+      try {
+        const query = `*[_type == "webImages"][0].logo`;
+        const logo = await client.fetch(query);
+
+        if (logo) {
+          let link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+          if (!link) {
+            link = document.createElement('link');
+            link.rel = 'icon';
+            document.getElementsByTagName('head')[0].appendChild(link);
+          }
+          link.href = urlFor(logo).url();
+        }
+      } catch (error) {
+        console.error("Error updating favicon:", error);
+      }
+    };
+
+    updateFavicon();
+  }, []);
   return (
     <Router>
       <div className="min-h-screen bg-white">
